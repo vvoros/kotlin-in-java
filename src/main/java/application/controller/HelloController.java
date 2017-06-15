8aa6b2f67a69282b4777b4481e92f1bad23f519f
Janos Csubakka <h836437@freemail.hu>
2017-06-15 21:25:27 +0200

    Kotlin content

--- src/main/java/application/controller/HelloController.java
+++ src/main/java/application/controller/HelloController.java
@@ -1,6 +1,7 @@
 package application.controller;
 
 import application.model.Hello;
+import application.model.HelloKotlin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +18,10 @@ public class HelloController {
   @RequestMapping("/hello")
   public Hello greeting(@RequestParam(defaultValue="World") String name) {
     return new Hello(counter.incrementAndGet(), String.format(HELLO_TEMPLATE, name));
+  }
+
+  @RequestMapping("/hello-kotlin")
+  public HelloKotlin greetingKotlin(@RequestParam(defaultValue="World") String name) {
+    return new HelloKotlin(counter.incrementAndGet(), String.format(HELLO_TEMPLATE, name));
   }
 }
---
+++ src/main/java/application/model/HelloKotlin.kt
@@ -0,0 +1,3 @@
+package application.model
+
+data class HelloKotlin(val id: Long, val content: String)
