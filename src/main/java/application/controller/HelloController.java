7b4a5341721cbee570824a91f927ac990a602f6b
Janos Csubakka <h836437@freemail.hu>
2017-06-15 21:15:14 +0200

    Simple java service

---
+++ src/main/java/application/controller/HelloController.java
@@ -0,0 +1,21 @@
+package application.controller;
+
+import application.model.Hello;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HelloController {
+
+  private static final String HELLO_TEMPLATE = "Hello, %s!";
+
+  private final AtomicLong counter = new AtomicLong();
+
+  @RequestMapping("/hello")
+  public Hello greeting(@RequestParam(defaultValue="World") String name) {
+    return new Hello(counter.incrementAndGet(), String.format(HELLO_TEMPLATE, name));
+  }
+}
